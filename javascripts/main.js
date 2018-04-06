d35714fcb13b33671520d6bafdcfefe9dcee5145
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-06 13:45:40 -0500

    added css

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,7 @@ const printToDom = (domString, divId) => {
 
 const buildDomString = () => {
   let domString = "";
-    domString +=  `<div class="form-group clearfix">`;
+    domString +=  `<div class="form-group">`;
     domString +=     `<div class="width pull-left">`;
     domString +=       `<label for="first-player">Player 1</label>`;
     domString +=       `<input type="text" class="form-control" id="first-player" placeholder="username">`;
@@ -16,13 +16,13 @@ const buildDomString = () => {
     domString +=       `<label for="second-player">Player 2</label>`;
     domString +=       `<input type="text" class="form-control" id="second-player" placeholder="username">`;
     domString +=     `</div>`;
-    domString +=     `<div class="col-xs-12">`;
-    domString +=       `<button type="button" id="button" class="btn btn-success center-block">Start Cage Match</button>`;
-    domString +=     `</div>`;
+    domString +=     `<button type="button" id="button" class="btn btn-success center-block">Start Cage Match</button>`;
     domString +=   `</div>`;
   printToDom(domString, "play");
   addEventListenerButton();
   }
+
+
 
 const addEventListenerButton = () => {
  let button = document.getElementById("button");
@@ -39,7 +39,7 @@ const initializeButton = (e) => {
   let inputPlayer2Value = inputPlayer2.value;
   xhr1(inputPlayer1Value);
   xhr1(inputPlayer2Value);
-  setTimeout(displayWinner, 2000);
+  setTimeout(displayWinner, 3000);
 }
 
 
@@ -73,10 +73,12 @@ const displayCageMatchResults = players => {
   let domString = "";
   // players.forEach(player => {
   // domString += `<div>`;
+
   domString +=   `<div class="col-sm-6">`;
   domString +=     `<div class="thumbnail center-block">`;
   domString +=       `<img src="${players.gravatar_url}" alt="...">`;
   domString +=       `<div class="caption">`;
+  domString +=         `<h5 class="text-center">${players.name}</h5>`;
   domString +=         `<h3 class="text-center">${players.points.total}</h3>`;
   domString +=       `</div>`;
   domString +=     `</div>`;
@@ -85,6 +87,8 @@ const displayCageMatchResults = players => {
   console.log
   printToDom(domString, "play");
 };
+
+
 
 const addPointstoArray = (playersData) => {
   const points1 = playersData.points.total;
@@ -99,43 +103,49 @@ const addPointstoArray = (playersData) => {
   
 }
 
+
+
 const displayWinner = () => {
   console.log(playersInfo);
   let domString = "";
   let winnerBadges=[];
   // for(var i=0; i<points.length; i++){
-    domString += `<div class="col-xs-12">`;
+    domString += `<div class="col-xs-12 margin-top-winnerHeading">`;
   if(playersInfo[0].points > playersInfo[1].points){
     winnerBadges.push(playersInfo[0].badges);
-    domString +=    `<h1 class="text-center">${playersInfo[0].name} is the WINNER</h1>`;
+    domString +=    `<h1 class="text-center" id="winner-heading">${playersInfo[0].name} is the WINNER</h1>`;
   } else if (playersInfo[0].points < playersInfo[1].points){
     winnerBadges.push(playersInfo[1].badges);
-    domString +=    `<h1 class="text-center">${playersInfo[1].name} is the WINNER</h1>`;
+    domString +=    `<h1 class="text-center" id="winner-heading">${playersInfo[1].name} is the WINNER</h1>`;
   }else {
-    domString +=    `<h1 class="text-center">Players have equal scores</h1>`;
+    domString +=    `<h1 class="text-center" id="winner-heading">Players have equal scores</h1>`;
   }
     domString += `</div>`;
   console.log(winnerBadges);
     // domString +=  `<div>`;
   winnerBadges[0].forEach((badge) => {
-  
-    // domString +=    `<div class=col-xs-1>`;
+    domString +=    `<div class="col-xs-3">`;
+    domString +=    `<div class="panel">`;
+    domString +=   `<div class="panel-heading">`;
+    domString +=      `<p class="text-center badge-name">${badge.name}</p>`;
+    domString +=   `</div>`;
+    domString +=   `<div class="panel-body">`;
     // domString +=      `<div class="pull-left badge-container">`;
-    domString +=        `<img class="img pull-left" src="${badge.icon_url}" alt="...">`; //class="img-responsive center-block"
+    domString +=        `<img class="img" src="${badge.icon_url}" alt="...">`; //class="img-responsive center-block"
     // domString +=        `<div class="caption">`;
-    // domString +=          `<p class="text-center badge-name">${badge.name}</p>`;
-    // domString +=        `</div>`;
+    domString +=   `</div>`;
+    domString += `</div>`;
+    domString += `</div>`;
     // domString +=    `</div>`;
   
     console.log(badge.icon_url);
   });
-    // domString +=  `</div>`;
-    
   printToDom(domString, "play");
   domString = "";
   console.log(winnerBadges);
   playersInfo =[];
 }
+  
 
 const startApplication = () => {
   buildDomString(); 
